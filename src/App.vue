30362106601f9f1c2a61a51d4756cd8fc2817c87
Santosh Casula <scasula@liveqos.com>
2018-11-01 16:03:32 -0400

    using setters

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
                 <hr>
                 <appAnotherResult></appAnotherResult>
                 <hr>
-                <input type="text" :value="value" @input="updateValue">
+                <input type="text" v-model="value">
                 <p>{{value}}</p>
             </div>
         </div>
@@ -25,8 +25,13 @@
     import AnotherResult from './components/AnotherResult.vue';
     export default {
         computed:{
-          value(){
-              return this.$store.getters.value;
+          value:{
+              get(){
+                  return this.$store.getters.value;
+              },
+              set(value){
+                  this.$store.dispatch('updateValue',value);
+              }
           }
         },
         methods:{
